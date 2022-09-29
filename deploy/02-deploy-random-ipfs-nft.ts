711eece98dbd4a32f8dca4b574738129557a1b7e
Vincent Howard <vincenguyenhoward@gmail.com>
2022-09-28 18:46:58 -0600

    added logic to populate pinata and deploy contract/chainlink contract

--- deploy/02-deploy-random-ipfs-nft.ts
+++ deploy/02-deploy-random-ipfs-nft.ts
@@ -1,11 +1,36 @@
 // packages
 import { ethers } from "hardhat";
 // utils
-import { developmentChains, networkConfig } from "../helper-hardhat-config";
+import {
+    developmentChains,
+    networkConfig,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} from "../helper-hardhat-config";
 import verify from "../utils/verify";
 // types
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { storeImages, storeTokeUriMetadata } from "../utils/uploadToPinata";
+
+const imagesLocation = "./images/randomDogs/";
+const FUND_AMOUNT = "1000000000000000000000";
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+];
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+};
 
 const deployRandomIpfsNft: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts, network } = hre;
@@ -13,6 +38,16 @@ const deployRandomIpfsNft: DeployFunction = async function (hre: HardhatRuntimeE
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId!;
 
+    // Get the IPFS hashes of images
+    // 1. With our own IPFS node. https://docs.ipfs.io/
+    // 2. Pinata. https://pinata.cloud/
+    // 3. NFT storage https://nft.storage/
+
+    if (Boolean(process.env.UPLOAD_TO_PINATA)) {
+        tokenUris = await handleTokenUris();
+    }
+
+    await storeImages(imagesLocation);
     let vrfCoordinatorV2Address, subscriptionId;
 
     if (developmentChains.includes(chainId.toString())) {
@@ -22,11 +57,61 @@ const deployRandomIpfsNft: DeployFunction = async function (hre: HardhatRuntimeE
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
         const transactionReceipt = await transactionResponse.wait(1);
         subscriptionId = transactionReceipt.events[0].args.subId;
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
     } else {
-        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
-        subscriptionId = networkConfig[chainId]["subscriptionId"];
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
+        subscriptionId = networkConfig[chainId].subscriptionId;
+    }
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane!,
+        networkConfig[chainId].mintFee!,
+        networkConfig[chainId].callbackGasLimit!,
+        tokenUris,
+    ];
+
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: waitBlockConfirmations || 1,
+    });
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...");
+        await verify(randomIpfsNft.address, args);
     }
 };
+
+async function handleTokenUris() {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+    tokenUris = [];
+
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation);
+    for (const imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate };
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "");
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`;
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`;
+        console.log(`Uploading ${tokenUriMetadata.name}...`);
+        const metadataUploadResponse = await storeTokeUriMetadata(tokenUriMetadata);
+        tokenUris.push(`ipfs://${metadataUploadResponse!.IpfsHash}`);
+    }
+    console.log("Token URIs uploaded! They are:");
+    console.log(tokenUris);
+    return tokenUris;
+}
 
 export default deployRandomIpfsNft;
 deployRandomIpfsNft.tags = ["all", "randomnft", "main"];
