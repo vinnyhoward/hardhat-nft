0c73e43e7ce54c714d584df2573464ae82a5e863
Vincent Howard <vincenguyenhoward@gmail.com>
2022-10-26 21:04:12 -0600

    getting back into learning moar solidity

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 import "hardhat/console.sol";
 
@@ -13,23 +14,27 @@ import "hardhat/console.sol";
 // Some logic to say "show X Image" or "show Y image"
 error DynamicSvgNft__YouRequireMoreMinerals(uint256 sent, uint256 required);
 
-contract DynamicSvgNft is ERC721 {
+contract DynamicSvgNft is ERC721, Ownable {
     uint256 private immutable i_mintFee;
     uint256 private s_tokenCounter;
-    string private i_lowImageUri;
-    string private i_highImageUri;
+    string private s_lowImageURI;
+    string private s_highImageURI;
 
     mapping(uint256 => int256) private s_tokenIdToHighValues;
-
+    AggregatorV3Interface internal immutable i_priceFeed;
     event CreatedNFT(uint256 indexed tokenId, int256 highValue);
 
     constructor(
+        address priceFeedAddress,
         uint256 mintFee,
         string memory lowSvg,
         string memory highSvg
     ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
         i_mintFee = mintFee;
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
@@ -38,18 +43,57 @@ contract DynamicSvgNft is ERC721 {
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
-    function mint() public payable {
+    function mint(int256 highValue) public payable {
         if (msg.value < i_mintFee) {
             revert DynamicSvgNft__YouRequireMoreMinerals({sent: msg.value, required: i_mintFee});
         }
 
-        _safeMint(msg.sender);
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
         s_tokenCounter = s_tokenCounter + 1;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit CreatedNFT(s_tokenCounter, highValue);
     }
 
     function getMintFee() public view returns (uint256) {
         return i_mintFee;
     }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+
+        string memory imageURI = s_lowImageURI;
+
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageURI;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    // append "data:application/json;base64"
+                    _baseURI(),
+                    // create hash for base64
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
 
     // TODO: Bonus function: Override ERC721 transfer to update mapping
 }
